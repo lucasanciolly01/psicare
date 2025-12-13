9e48a5f3af74320530fe58ea2a03a60fdfc4f616
lucasanciolly01 <lanciolly17@gmail.com>
2025-12-13 12:37:33 -0300

    a

--- psicare-frontend/src/main.tsx
+++ psicare-frontend/src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css' // Mantenha apenas este CSS
-// import './App.css'  <-- SE TIVER ISSO, APAGUE ESTA LINHA!
+import './index.css'
+import { ErrorBoundary } from 'react-error-boundary'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary fallback={<div>Algo deu errado!</div>}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
